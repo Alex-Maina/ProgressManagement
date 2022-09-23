f800b33ba331d60a8129cdda167e546382623a52
Alex-Maina <kalexmaina@gmail.com>
2022-09-24 00:01:26 +0300

    Facade

---
+++ PTSLibrary/Facades/SuperFacade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSLibrary.Facades
+{
+    public class SuperFacade : MarshalByRefObject
+    {
+        protected DataAccess.SuperDAO dao;
+
+        public SuperFacade(DataAccess.SuperDAO dao)
+        {
+            this.dao = dao;
+        }
+
+        
+    }
+}
