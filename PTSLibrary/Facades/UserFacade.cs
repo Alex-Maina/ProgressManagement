a04116f2109efcd0a375d75aeb3ce0d5cf6ce9b3
Alex-Maina <kalexmaina@gmail.com>
2022-09-24 23:56:10 +0300

    UserFacade fixed

---
+++ PTSLibrary/Facades/UserFacade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PTSLibrary.Models;
+
+namespace PTSLibrary.Facades
+{
+    internal class UserFacade : SuperFacade
+    {
+        private new DataAccess.UserDAO dao;
+
+        public UserFacade() : base(new DataAccess.UserDAO())
+        {
+            dao = (DataAccess.UserDAO)base.dao;
+        }
+        public UserModel Authenticate(string email, string password)
+        {
+            if (email == null || password == null)
+            {
+                throw new Exception("Missing Data");
+            }
+            return dao.Authenticate(email, password);
+        }
+
+        public ProjectModel[] GetListOfProjects(int teamId)
+        {
+            return (dao.GetListOfProjects(teamId).ToArray());
+        }
+    }
+}
