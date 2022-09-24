20193b76965839cfa6951992e43be1acd7116e97
Alex-Maina <kalexmaina@gmail.com>
2022-09-25 02:05:48 +0300

    list projects

--- PTSLibrary/DataAccess/SuperDAO.cs
+++ PTSLibrary/DataAccess/SuperDAO.cs
@@ -43,8 +43,41 @@ namespace PTSLibrary.DataAccess
             return user;
         }
 
+        //List of the projects
+        public List<ProjectModel> GetListOfProjects()
+        {
+            string sql;
+            SqlConnection con = new(Properties.Settings.Default.PTSConnectionstring);
+            SqlCommand cmd;
+            SqlDataReader dr;
+            List<ProjectModel> projects;
+            projects = new List<ProjectModel>();
+            sql = "SELECT * FROM project";
+            cmd = new SqlCommand(sql, con);
+            try
+            {
+                con.Open();
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ProjectModel p = new(dr["ProjectCode"].ToString(), dr["ProjectName"].ToString());
+                    projects.Add(p);
+                }
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Getting list", ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return projects;
+        }
+
         //List the tasks
-        public List<TaskModel> GetListOfTasks(int ID)
+        public List<TaskModel> GetListOfTasks(int projectID)
         {
             string sql;
             SqlConnection con = new(Properties.Settings.Default.PTSConnectionstring);
@@ -52,7 +85,7 @@ namespace PTSLibrary.DataAccess
             SqlDataReader dr;
             List<TaskModel> tasks;
             tasks = new List<TaskModel>();
-            sql = "SELECT * FROM Tasks WHERE ProjectID = '" + ID + "'";
+            sql = "SELECT * FROM Tasks WHERE ProjectID = '" + projectID + "'";
             cmd = new SqlCommand(sql, con);
 
             try
