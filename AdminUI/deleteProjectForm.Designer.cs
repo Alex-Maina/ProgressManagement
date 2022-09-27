a1cf2eb8d0767a3e03826d34ad4533b435c7eec1
Alex-Maina <kalexmaina@gmail.com>
2022-09-28 01:12:23 +0300

    Delete proj

---
+++ AdminUI/deleteProjectForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AdminUI
+{
+    partial class deleteProjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Are you sure you want to delete";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(299, 118);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(0, 25);
+            this.nameLabel.TabIndex = 1;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.deleteBtn.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.deleteBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.deleteBtn.Location = new System.Drawing.Point(341, 179);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(165, 42);
+            this.deleteBtn.TabIndex = 2;
+            this.deleteBtn.Text = "Yes, delete";
+            this.deleteBtn.UseVisualStyleBackColor = false;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(150, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "No, abort";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // deleteProjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(659, 287);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "deleteProjectForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "deleteProjectForm";
+            this.Load += new System.EventHandler(this.deleteProjectForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label nameLabel;
+        private Button deleteBtn;
+        private Button button1;
+    }
+}
---
+++ AdminUI/deleteProjectForm.cs
@@ -0,0 +1,49 @@
+﻿using PTSLibrary.Facades;
+using PTSLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminUI
+{
+    public partial class deleteProjectForm : Form
+    {
+        private AdminFacade facade;
+        int ID = HomeForm.selectedProject.ProjectID;
+
+        public deleteProjectForm()
+        {
+            InitializeComponent();
+            facade = new AdminFacade();
+        }
+
+        private void deleteProjectForm_Load(object sender, EventArgs e)
+        {
+            nameLabel.Text = HomeForm.selectedProject.ProjectName+" ?";
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            facade.DeleteProject(ID);
+            //Reopen the updated window
+            HomeForm home = new();
+            home.Show();
+            MessageBox.Show("Project deleted successfuly");
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Reopen the updated window
+            HomeForm home = new();
+            home.Show();
+            Close();
+        }
+    }
+}
