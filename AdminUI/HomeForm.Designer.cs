83d5452b034721efe6c892a2692e3af62a246ee9
Alex-Maina <kalexmaina@gmail.com>
2022-09-28 01:12:55 +0300

    Dashbord

--- AdminUI/HomeForm.Designer.cs
+++ AdminUI/HomeForm.Designer.cs
@@ -31,13 +31,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.tabControl = new System.Windows.Forms.TabControl();
             this.projectsTabPage = new System.Windows.Forms.TabPage();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.viewBtn = new System.Windows.Forms.Button();
             this.projectsListBox = new System.Windows.Forms.ListBox();
-            this.assignButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.EditButton = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.tabControl.SuspendLayout();
             this.projectsTabPage.SuspendLayout();
             this.SuspendLayout();
@@ -48,9 +48,9 @@
             this.label1.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(12, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 33);
+            this.label1.Size = new System.Drawing.Size(251, 33);
             this.label1.TabIndex = 0;
-            this.label1.Text = "SEPA PTS";
+            this.label1.Text = "Admin Dashboard";
             // 
             // tabControl
             // 
@@ -58,69 +58,94 @@
             this.tabControl.Controls.Add(this.tabPage2);
             this.tabControl.Controls.Add(this.tabPage1);
             this.tabControl.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tabControl.HotTrack = true;
             this.tabControl.Location = new System.Drawing.Point(0, 80);
             this.tabControl.Multiline = true;
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(1042, 528);
+            this.tabControl.Size = new System.Drawing.Size(1042, 531);
             this.tabControl.TabIndex = 1;
             // 
             // projectsTabPage
             // 
             this.projectsTabPage.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.projectsTabPage.Controls.Add(this.addBtn);
+            this.projectsTabPage.Controls.Add(this.deleteBtn);
+            this.projectsTabPage.Controls.Add(this.viewBtn);
             this.projectsTabPage.Controls.Add(this.projectsListBox);
-            this.projectsTabPage.Controls.Add(this.button4);
-            this.projectsTabPage.Controls.Add(this.EditButton);
-            this.projectsTabPage.Controls.Add(this.assignButton);
-            this.projectsTabPage.Controls.Add(this.button1);
+            this.projectsTabPage.Controls.Add(this.editBtn);
             this.projectsTabPage.Location = new System.Drawing.Point(4, 34);
             this.projectsTabPage.Name = "projectsTabPage";
             this.projectsTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.projectsTabPage.Size = new System.Drawing.Size(1034, 490);
+            this.projectsTabPage.Size = new System.Drawing.Size(1034, 493);
             this.projectsTabPage.TabIndex = 0;
             this.projectsTabPage.Text = "Projects      ";
             this.projectsTabPage.Click += new System.EventHandler(this.projectsTabPage_Click);
             // 
+            // addBtn
+            // 
+            this.addBtn.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addBtn.Location = new System.Drawing.Point(848, 340);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(149, 43);
+            this.addBtn.TabIndex = 4;
+            this.addBtn.Text = "+ New Project";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.deleteBtn.Location = new System.Drawing.Point(848, 190);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(154, 43);
+            this.deleteBtn.TabIndex = 3;
+            this.deleteBtn.Text = "Delete Project";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // viewBtn
+            // 
+            this.viewBtn.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.viewBtn.Location = new System.Drawing.Point(848, 47);
+            this.viewBtn.Name = "viewBtn";
+            this.viewBtn.Size = new System.Drawing.Size(154, 48);
+            this.viewBtn.TabIndex = 2;
+            this.viewBtn.Text = "View Project";
+            this.viewBtn.UseVisualStyleBackColor = true;
+            this.viewBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
             // projectsListBox
             // 
-            this.projectsListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.projectsListBox.AllowDrop = true;
+            this.projectsListBox.BackColor = System.Drawing.SystemColors.Window;
+            this.projectsListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.projectsListBox.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.projectsListBox.FormattingEnabled = true;
             this.projectsListBox.ItemHeight = 25;
-            this.projectsListBox.Location = new System.Drawing.Point(19, 34);
+            this.projectsListBox.Location = new System.Drawing.Point(31, 34);
             this.projectsListBox.Name = "projectsListBox";
-            this.projectsListBox.Size = new System.Drawing.Size(777, 377);
+            this.projectsListBox.Size = new System.Drawing.Size(682, 350);
             this.projectsListBox.TabIndex = 1;
             this.projectsListBox.SelectedIndexChanged += new System.EventHandler(this.projectsListBox_SelectedIndexChanged);
             // 
-            // assignButton
+            // editBtn
             // 
-            this.assignButton.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.assignButton.Location = new System.Drawing.Point(843, 77);
-            this.assignButton.Name = "assignButton";
-            this.assignButton.Size = new System.Drawing.Size(154, 43);
-            this.assignButton.TabIndex = 0;
-            this.assignButton.Text = "Assign Project";
-            this.assignButton.UseVisualStyleBackColor = true;
-            this.assignButton.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(19, 428);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "+ New Project";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.editBtn.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.editBtn.Location = new System.Drawing.Point(848, 122);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(154, 43);
+            this.editBtn.TabIndex = 0;
+            this.editBtn.Text = "Edit Project";
+            this.editBtn.UseVisualStyleBackColor = true;
+            this.editBtn.Click += new System.EventHandler(this.button1_Click);
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 34);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1034, 490);
+            this.tabPage2.Size = new System.Drawing.Size(1034, 493);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Cohorts      ";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -130,32 +155,10 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 34);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1034, 468);
+            this.tabPage1.Size = new System.Drawing.Size(1034, 493);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "Teamleaders      ";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // EditButton
-            // 
-            this.EditButton.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.EditButton.Location = new System.Drawing.Point(843, 151);
-            this.EditButton.Name = "EditButton";
-            this.EditButton.Size = new System.Drawing.Size(154, 43);
-            this.EditButton.TabIndex = 0;
-            this.EditButton.Text = "Edit Project";
-            this.EditButton.UseVisualStyleBackColor = true;
-            this.EditButton.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button4.Location = new System.Drawing.Point(843, 225);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(154, 43);
-            this.button4.TabIndex = 0;
-            this.button4.Text = "Delete Project";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button1_Click);
             // 
             // HomeForm
             // 
@@ -163,13 +166,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.AliceBlue;
-            this.ClientSize = new System.Drawing.Size(1041, 608);
+            this.ClientSize = new System.Drawing.Size(1041, 576);
             this.Controls.Add(this.tabControl);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.Name = "HomeForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "HomeForm";
+            this.Text = "SEPA PTS";
             this.tabControl.ResumeLayout(false);
             this.projectsTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -184,10 +187,10 @@
         private TabPage projectsTabPage;
         private TabPage tabPage2;
         private TabPage tabPage1;
-        private Button button1;
-        private Button assignButton;
         private ListBox projectsListBox;
-        private Button button4;
-        private Button EditButton;
+        private Button editBtn;
+        private Button viewBtn;
+        private Button deleteBtn;
+        private Button addBtn;
     }
 }
--- AdminUI/HomeForm.cs
+++ AdminUI/HomeForm.cs
@@ -12,18 +12,18 @@ using System.Windows.Forms;
 
 namespace AdminUI
 {
-    public partial class HomeForm : Form
+    public partial class HomeForm : System.Windows.Forms.Form
     {
         private AdminFacade facade;
         private int Id;
 
         private UserModel[] users;
-        private ProjectModel[] projects;
+        public static ProjectModel[] projects;
         private CohortModel[] cohort;
-        private ProjectModel selectedProject;
+        public static ProjectModel selectedProject;
         private TaskModel[] tasks;
 
-        
+
 
         public HomeForm()
         {
@@ -37,23 +37,57 @@ namespace AdminUI
         {
             
         }
-
+        //View btn event
+        private void button2_Click(object sender, EventArgs e)
+        {
+            selectedProject = projects[projectsListBox.SelectedIndex];
+            projectForm view = new();
+            view.Show();
+        }
+        //Edit btn event
         private void button1_Click(object sender, EventArgs e)
         {
-
+            selectedProject = projects[projectsListBox.SelectedIndex];
+            editProjectForm edit = new();
+            edit.Show();
+            Close();
         }
-
-        private void DisplayProjects()
+        //Delete btn event
+        private void button2_Click_1(object sender, EventArgs e)
         {
-            projects = facade.GetListOfProjects();
-            projectsListBox.DataSource = projects;
-            projectsListBox.DisplayMember = "DisplayProject";
-            projectsListBox.ValueMember = "ProjectCode";
+            selectedProject = projects[projectsListBox.SelectedIndex];
+            deleteProjectForm delete = new();
+            delete.Show();
+            Close();
+        }
+        //Add new project
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            addProjectForm add = new();
+            add.Show();
+            Close();
         }
 
         private void projectsListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        //Display list of projects
+        public void DisplayProjects()
+        {
+            projects = facade.GetListOfProjects();
+            projectsListBox.DataSource = projects;
+            projectsListBox.DisplayMember = "DisplayProject";
+            projectsListBox.ValueMember = "ProjectID";
+        }
+
+        //Display details of selected project
+        private void viewProject()
+        {
+            selectedProject = projects[projectsListBox.SelectedIndex];
+            projectForm ob = new();
+            ob.Show();
         }
     }
 }
