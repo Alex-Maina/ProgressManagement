dbfa90eededd606d012e4c39bd2e2343be63e2fd
Alex-Maina <kalexmaina@gmail.com>
2022-09-28 01:13:42 +0300

    view proj

---
+++ AdminUI/projectViewForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace AdminUI
+{
+    partial class projectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.codeLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.levelLabel = new System.Windows.Forms.Label();
+            this.githubLabel = new System.Windows.Forms.Label();
+            this.videoLabel = new System.Windows.Forms.Label();
+            this.durationLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.descriptionBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(60, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project Code : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(60, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Project Name : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(60, 337);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Description : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(60, 184);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Project Level : ";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // codeLabel
+            // 
+            this.codeLabel.AutoSize = true;
+            this.codeLabel.Location = new System.Drawing.Point(225, 135);
+            this.codeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.codeLabel.Name = "codeLabel";
+            this.codeLabel.Size = new System.Drawing.Size(0, 25);
+            this.codeLabel.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(469, 135);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(208, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Estimated Duration : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(60, 228);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 25);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Repository : ";
+            this.label7.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(60, 275);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 25);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Video Link : ";
+            this.label8.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // levelLabel
+            // 
+            this.levelLabel.AutoSize = true;
+            this.levelLabel.Location = new System.Drawing.Point(225, 184);
+            this.levelLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.levelLabel.Name = "levelLabel";
+            this.levelLabel.Size = new System.Drawing.Size(0, 25);
+            this.levelLabel.TabIndex = 1;
+            // 
+            // githubLabel
+            // 
+            this.githubLabel.AutoSize = true;
+            this.githubLabel.Location = new System.Drawing.Point(225, 228);
+            this.githubLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.githubLabel.Name = "githubLabel";
+            this.githubLabel.Size = new System.Drawing.Size(0, 25);
+            this.githubLabel.TabIndex = 1;
+            // 
+            // videoLabel
+            // 
+            this.videoLabel.AutoSize = true;
+            this.videoLabel.Location = new System.Drawing.Point(225, 275);
+            this.videoLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.videoLabel.Name = "videoLabel";
+            this.videoLabel.Size = new System.Drawing.Size(0, 25);
+            this.videoLabel.TabIndex = 1;
+            // 
+            // durationLabel
+            // 
+            this.durationLabel.AutoSize = true;
+            this.durationLabel.Location = new System.Drawing.Point(685, 135);
+            this.durationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.durationLabel.Name = "durationLabel";
+            this.durationLabel.Size = new System.Drawing.Size(0, 25);
+            this.durationLabel.TabIndex = 1;
+            this.durationLabel.Click += new System.EventHandler(this.durationLabel_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Montserrat SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nameLabel.Location = new System.Drawing.Point(225, 52);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(264, 27);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Project Tracking System";
+            this.nameLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // descriptionBox
+            // 
+            this.descriptionBox.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.descriptionBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.descriptionBox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.descriptionBox.Location = new System.Drawing.Point(225, 337);
+            this.descriptionBox.Name = "descriptionBox";
+            this.descriptionBox.ReadOnly = true;
+            this.descriptionBox.Size = new System.Drawing.Size(698, 229);
+            this.descriptionBox.TabIndex = 2;
+            this.descriptionBox.Text = "";
+            // 
+            // projectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(960, 599);
+            this.Controls.Add(this.descriptionBox);
+            this.Controls.Add(this.durationLabel);
+            this.Controls.Add(this.videoLabel);
+            this.Controls.Add(this.githubLabel);
+            this.Controls.Add(this.levelLabel);
+            this.Controls.Add(this.codeLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "projectForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.projectViewForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label codeLabel;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label levelLabel;
+        private Label githubLabel;
+        private Label videoLabel;
+        private Label durationLabel;
+        private Label nameLabel;
+        private RichTextBox descriptionBox;
+    }
+}
---
+++ AdminUI/projectViewForm.cs
@@ -0,0 +1,45 @@
+﻿using PTSLibrary.Facades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminUI
+{
+    public partial class projectForm : System.Windows.Forms.Form
+    {
+
+        public projectForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void durationLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void projectViewForm_Load(object sender, EventArgs e)
+        {
+            Text = HomeForm.selectedProject.ProjectName;
+            nameLabel.Text = HomeForm.selectedProject.ProjectName;
+            codeLabel.Text = HomeForm.selectedProject.ProjectCode;
+            durationLabel.Text = HomeForm.selectedProject.ProjectDuration.ToString() + " weeks";
+            levelLabel.Text = HomeForm.selectedProject.Level;
+            videoLabel.Text = HomeForm.selectedProject.Link;
+            githubLabel.Text = HomeForm.selectedProject.Github;
+            descriptionBox.Text = HomeForm.selectedProject.ProjectDescription;
+        }
+    }
+}
