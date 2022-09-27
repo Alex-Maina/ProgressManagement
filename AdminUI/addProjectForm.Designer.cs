da0da13729599735f2a770cbf5c97cfed706ed3f
Alex-Maina <kalexmaina@gmail.com>
2022-09-28 01:12:10 +0300

    Add proj

---
+++ AdminUI/addProjectForm.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace AdminUI
+{
+    partial class addProjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.durationBox = new System.Windows.Forms.TextBox();
+            this.videoBox = new System.Windows.Forms.TextBox();
+            this.repoBox = new System.Windows.Forms.TextBox();
+            this.descriptionBox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.levelBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nameBox.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nameBox.Location = new System.Drawing.Point(331, 54);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(557, 29);
+            this.nameBox.TabIndex = 37;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.BackColor = System.Drawing.Color.Transparent;
+            this.cancelBtn.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cancelBtn.Location = new System.Drawing.Point(637, 584);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(112, 34);
+            this.cancelBtn.TabIndex = 36;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = false;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.Color.SkyBlue;
+            this.addBtn.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addBtn.Location = new System.Drawing.Point(776, 584);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(112, 34);
+            this.addBtn.TabIndex = 35;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = false;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(368, 114);
+            this.label5.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 25);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "weeks";
+            // 
+            // durationBox
+            // 
+            this.durationBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.durationBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.durationBox.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.durationBox.Location = new System.Drawing.Point(312, 112);
+            this.durationBox.Name = "durationBox";
+            this.durationBox.Size = new System.Drawing.Size(50, 29);
+            this.durationBox.TabIndex = 32;
+            // 
+            // videoBox
+            // 
+            this.videoBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.videoBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.videoBox.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.videoBox.Location = new System.Drawing.Point(250, 217);
+            this.videoBox.Name = "videoBox";
+            this.videoBox.Size = new System.Drawing.Size(638, 29);
+            this.videoBox.TabIndex = 31;
+            // 
+            // repoBox
+            // 
+            this.repoBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.repoBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.repoBox.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.repoBox.Location = new System.Drawing.Point(250, 170);
+            this.repoBox.Name = "repoBox";
+            this.repoBox.Size = new System.Drawing.Size(638, 29);
+            this.repoBox.TabIndex = 30;
+            // 
+            // descriptionBox
+            // 
+            this.descriptionBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.descriptionBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.descriptionBox.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.descriptionBox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.descriptionBox.Location = new System.Drawing.Point(250, 283);
+            this.descriptionBox.Name = "descriptionBox";
+            this.descriptionBox.Size = new System.Drawing.Size(638, 264);
+            this.descriptionBox.TabIndex = 28;
+            this.descriptionBox.Text = "";
+            this.descriptionBox.TextChanged += new System.EventHandler(this.descriptionBox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(85, 283);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 25);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Description * : ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(85, 221);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 25);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Video Link : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(85, 174);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 25);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Repository : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(536, 114);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 25);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Project Level * : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(85, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 27);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Enter Project Name * :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(85, 114);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(220, 25);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Estimated Duration * : ";
+            // 
+            // levelBox
+            // 
+            this.levelBox.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.levelBox.FormattingEnabled = true;
+            this.levelBox.Items.AddRange(new object[] {
+            "Beginner",
+            "Intermediate",
+            "Advance"});
+            this.levelBox.Location = new System.Drawing.Point(701, 111);
+            this.levelBox.Name = "levelBox";
+            this.levelBox.Size = new System.Drawing.Size(187, 33);
+            this.levelBox.TabIndex = 38;
+            this.levelBox.Text = "Beginner";
+            // 
+            // addProjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 650);
+            this.ControlBox = false;
+            this.Controls.Add(this.levelBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.durationBox);
+            this.Controls.Add(this.videoBox);
+            this.Controls.Add(this.repoBox);
+            this.Controls.Add(this.descriptionBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Name = "addProjectForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox nameBox;
+        private Button cancelBtn;
+        private Button addBtn;
+        private Label label5;
+        private TextBox durationBox;
+        private TextBox videoBox;
+        private TextBox repoBox;
+        private RichTextBox descriptionBox;
+        private Label label3;
+        private Label label8;
+        private Label label7;
+        private Label label4;
+        private Label label2;
+        private Label label6;
+        private ComboBox levelBox;
+    }
+}
---
+++ AdminUI/addProjectForm.cs
@@ -0,0 +1,79 @@
+﻿using PTSLibrary.Facades;
+using PTSLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminUI
+{
+    public partial class addProjectForm : Form
+    {
+        private AdminFacade facade;
+        
+        public addProjectForm()
+        {
+            InitializeComponent();
+            facade = new AdminFacade();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int duration;
+                bool isParsable = Int32.TryParse(durationBox.Text, out duration);
+                if (isParsable)
+                {
+                    facade.CreateProject(
+                                nameBox.Text,
+                                descriptionBox.Text,
+                                levelBox.Text,
+                                duration,
+                                repoBox.Text,
+                                videoBox.Text
+                           );
+
+                    nameBox.Text = "";
+                    durationBox.Text = "";
+                    levelBox.Text = "";
+                    videoBox.Text = "";
+                    repoBox.Text = "";
+                    descriptionBox.Text = "";
+
+                    MessageBox.Show("Project added successfuly");
+
+                    //Reopen the updated window
+                    HomeForm home = new();
+                    home.Show();
+                    //Close this window
+                    Close();
+                }
+                else
+                    MessageBox.Show("Duration should be a number");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            //Reopen the updated window
+            HomeForm home = new();
+            home.Show();
+            Close();
+        }
+
+        private void descriptionBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
