4568b1e1bd787dfadce384f0f4d3885e332315f4
Alex-Maina <kalexmaina@gmail.com>
2022-09-29 23:24:41 +0300

    Cohort controls (Admin Form)

---
+++ AdminUI/addCohortForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace AdminUI
+{
+    partial class addCohortForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startDate = new System.Windows.Forms.DateTimePicker();
+            this.addCohortBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(66, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(420, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Provide a new start date for the new cohort";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(66, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Start Date :";
+            // 
+            // startDate
+            // 
+            this.startDate.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.startDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.startDate.Location = new System.Drawing.Point(189, 122);
+            this.startDate.MinDate = new System.DateTime(2022, 9, 28, 0, 0, 0, 0);
+            this.startDate.Name = "startDate";
+            this.startDate.Size = new System.Drawing.Size(185, 29);
+            this.startDate.TabIndex = 3;
+            this.startDate.Value = new System.DateTime(2022, 9, 28, 13, 35, 32, 0);
+            // 
+            // addCohortBtn
+            // 
+            this.addCohortBtn.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addCohortBtn.Location = new System.Drawing.Point(66, 213);
+            this.addCohortBtn.Name = "addCohortBtn";
+            this.addCohortBtn.Size = new System.Drawing.Size(128, 34);
+            this.addCohortBtn.TabIndex = 4;
+            this.addCohortBtn.Text = "Create New";
+            this.addCohortBtn.UseVisualStyleBackColor = true;
+            this.addCohortBtn.Click += new System.EventHandler(this.addCohortBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Font = new System.Drawing.Font("Montserrat SemiBold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cancelBtn.Location = new System.Drawing.Point(212, 213);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(112, 34);
+            this.cancelBtn.TabIndex = 5;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // addCohortForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 290);
+            this.ControlBox = false;
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.addCohortBtn);
+            this.Controls.Add(this.startDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "addCohortForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add New Cohort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private DateTimePicker startDate;
+        private Button addCohortBtn;
+        private Button cancelBtn;
+    }
+}
---
+++ AdminUI/addCohortForm.cs
@@ -0,0 +1,44 @@
+﻿using PTSLibrary.Facades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminUI
+{
+    public partial class addCohortForm : Form
+    {
+        private AdminFacade facade;
+        public addCohortForm()
+        {
+            InitializeComponent();
+            facade = new AdminFacade();
+        }
+
+        private void addCohortBtn_Click(object sender, EventArgs e)
+        {
+            facade.CreateCohort(startDate.Value);
+
+            MessageBox.Show("New cohort created successfuly");
+
+            //Reopen the updated window
+            HomeForm home = new();
+            home.Show();
+            //Close this window
+            Close();
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            //Reopen the updated window
+            HomeForm home = new();
+            home.Show();
+            Close();
+        }
+    }
+}
