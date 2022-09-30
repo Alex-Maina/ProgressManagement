42117c7e888938b299212c6e3513f68e83f15798
Alex-Maina <kalexmaina@gmail.com>
2022-09-30 22:02:23 +0300

    graduateCohortForm  added

---
+++ AdminUI/graduateCohortForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AdminUI
+{
+    partial class graduateCohortForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.confirmBtn = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.cancelBtn.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cancelBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.cancelBtn.Location = new System.Drawing.Point(263, 124);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(165, 42);
+            this.cancelBtn.TabIndex = 11;
+            this.cancelBtn.Text = "No, abort";
+            this.cancelBtn.UseVisualStyleBackColor = false;
+            this.cancelBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // confirmBtn
+            // 
+            this.confirmBtn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.confirmBtn.Font = new System.Drawing.Font("Montserrat Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.confirmBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.confirmBtn.Location = new System.Drawing.Point(69, 124);
+            this.confirmBtn.Name = "confirmBtn";
+            this.confirmBtn.Size = new System.Drawing.Size(165, 42);
+            this.confirmBtn.TabIndex = 10;
+            this.confirmBtn.Text = "Yes, Confirm";
+            this.confirmBtn.UseVisualStyleBackColor = false;
+            this.confirmBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Montserrat SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nameLabel.Location = new System.Drawing.Point(69, 23);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(77, 25);
+            this.nameLabel.TabIndex = 9;
+            this.nameLabel.Text = "Cohort";
+            this.nameLabel.Click += new System.EventHandler(this.nameLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(69, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(486, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Confirm that this cohort has completed the program";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // graduateCohortForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 197);
+            this.ControlBox = false;
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.confirmBtn);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "graduateCohortForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Confirm Graduation";
+            this.Load += new System.EventHandler(this.graduateCohortForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button cancelBtn;
+        private Button confirmBtn;
+        private Label nameLabel;
+        private Label label1;
+    }
+}
---
+++ AdminUI/graduateCohortForm.cs
@@ -0,0 +1,57 @@
+﻿using PTSLibrary.Facades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminUI
+{
+    public partial class graduateCohortForm : Form
+    {
+        private AdminFacade facade;
+        int id = HomeForm.selectedCohort.CohortID;
+
+        public graduateCohortForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            facade.graduateCohort(id);
+            //Reopen the updated window
+            HomeForm home = new();
+            home.Show();
+            MessageBox.Show("Completion confirmed");
+            Close();
+        }
+
+        private void nameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Reopen the updated window
+            HomeForm home = new();
+            home.Show();
+            Close();
+        }
+
+        private void graduateCohortForm_Load(object sender, EventArgs e)
+        {
+            nameLabel.Text = HomeForm.selectedCohort.CohortName;
+        }
+    }
+}
