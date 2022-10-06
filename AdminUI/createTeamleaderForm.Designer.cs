0628d11341c667b1e95b7f9e4bc433ef5526a29e
Alex-Maina <kalexmaina@gmail.com>
2022-10-06 16:12:44 +0300

    create teamleader

---
+++ AdminUI/createTeamleaderForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace AdminUI
+{
+    partial class createTeamleaderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(101, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(101, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(101, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phone Number :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(101, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Email Address :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(340, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.Location = new System.Drawing.Point(457, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ABORT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(288, 47);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(267, 31);
+            this.fname.TabIndex = 4;
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(288, 123);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(267, 31);
+            this.lname.TabIndex = 4;
+            // 
+            // phone
+            // 
+            this.phone.Location = new System.Drawing.Point(288, 190);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(267, 31);
+            this.phone.TabIndex = 4;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(288, 261);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(267, 31);
+            this.email.TabIndex = 4;
+            // 
+            // createTeamleaderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 420);
+            this.ControlBox = false;
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "createTeamleaderForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create Teamleader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private TextBox fname;
+        private TextBox lname;
+        private TextBox phone;
+        private TextBox email;
+    }
+}
---
+++ AdminUI/createTeamleaderForm.cs
@@ -0,0 +1,60 @@
+﻿using PTSLibrary.Facades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace AdminUI
+{
+    public partial class createTeamleaderForm : Form
+    {
+        private AdminFacade facade;
+
+        public createTeamleaderForm()
+        {
+            InitializeComponent();
+            facade = new AdminFacade();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HomeForm home = new();
+            home.Show();
+            //Close this window
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                facade.CreateTeamleader(
+                    fname.Text,
+                    lname.Text,
+                    phone.Text,
+                    email.Text
+                 );
+                fname.Text = "";
+                lname.Text = "";
+                phone.Text = "";
+                email.Text = "";
+                
+                MessageBox.Show("Teamleader created successfuly");
+                HomeForm home = new();
+                home.Show();
+                //Close this window
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
