4b2e7c829e459d03a5e949f379419a8c4479c9ae
Alex-Maina <kalexmaina@gmail.com>
2022-10-07 20:18:16 +0300

    assign project

--- AdminUI/assignProjectForm.Designer.cs
+++ AdminUI/assignProjectForm.Designer.cs
@@ -32,39 +32,41 @@
             this.button1 = new System.Windows.Forms.Button();
             this.projectComboBox = new System.Windows.Forms.ComboBox();
             this.teamleaderComboBox = new System.Windows.Forms.ComboBox();
-            this.startdate = new System.Windows.Forms.DateTimePicker();
+            this.startDateBox = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.cohortLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.IndianRed;
-            this.button2.Location = new System.Drawing.Point(460, 350);
+            this.button2.Location = new System.Drawing.Point(561, 346);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(98, 34);
             this.button2.TabIndex = 11;
             this.button2.Text = "Abort";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.button1.Location = new System.Drawing.Point(356, 350);
+            this.button1.Location = new System.Drawing.Point(432, 346);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 34);
             this.button1.TabIndex = 12;
             this.button1.Text = "Assign";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // projectComboBox
             // 
             this.projectComboBox.FormattingEnabled = true;
             this.projectComboBox.Location = new System.Drawing.Point(303, 123);
             this.projectComboBox.Name = "projectComboBox";
-            this.projectComboBox.Size = new System.Drawing.Size(255, 33);
+            this.projectComboBox.Size = new System.Drawing.Size(356, 33);
             this.projectComboBox.TabIndex = 9;
             // 
             // teamleaderComboBox
@@ -72,17 +74,17 @@
             this.teamleaderComboBox.FormattingEnabled = true;
             this.teamleaderComboBox.Location = new System.Drawing.Point(303, 190);
             this.teamleaderComboBox.Name = "teamleaderComboBox";
-            this.teamleaderComboBox.Size = new System.Drawing.Size(255, 33);
+            this.teamleaderComboBox.Size = new System.Drawing.Size(356, 33);
             this.teamleaderComboBox.TabIndex = 10;
             // 
-            // startdate
+            // startDateBox
             // 
-            this.startdate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.startdate.Location = new System.Drawing.Point(303, 259);
-            this.startdate.Name = "startdate";
-            this.startdate.Size = new System.Drawing.Size(255, 31);
-            this.startdate.TabIndex = 8;
-            this.startdate.Value = new System.DateTime(2022, 10, 6, 14, 49, 35, 0);
+            this.startDateBox.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.startDateBox.Location = new System.Drawing.Point(303, 259);
+            this.startDateBox.Name = "startDateBox";
+            this.startDateBox.Size = new System.Drawing.Size(356, 31);
+            this.startDateBox.TabIndex = 8;
+            this.startDateBox.Value = new System.DateTime(2022, 10, 7, 0, 0, 0, 0);
             // 
             // label4
             // 
@@ -114,34 +116,35 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Select project :";
             // 
-            // label1
+            // cohortLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(104, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 25);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Cohort ";
+            this.cohortLabel.AutoSize = true;
+            this.cohortLabel.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cohortLabel.Location = new System.Drawing.Point(104, 50);
+            this.cohortLabel.Name = "cohortLabel";
+            this.cohortLabel.Size = new System.Drawing.Size(128, 25);
+            this.cohortLabel.TabIndex = 7;
+            this.cohortLabel.Text = "Cohort Label";
             // 
             // assignProjectForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(702, 441);
+            this.ClientSize = new System.Drawing.Size(781, 441);
             this.ControlBox = false;
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.projectComboBox);
             this.Controls.Add(this.teamleaderComboBox);
-            this.Controls.Add(this.startdate);
+            this.Controls.Add(this.startDateBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cohortLabel);
             this.Name = "assignProjectForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Assign Project";
+            this.Load += new System.EventHandler(this.assignProjectForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,6 +160,7 @@
         private Label label4;
         private Label label3;
         private Label label2;
-        private Label label1;
+        private Label cohortLabel;
+        private DateTimePicker startDateBox;
     }
 }
--- AdminUI/assignProjectForm.cs
+++ AdminUI/assignProjectForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PTSLibrary.Facades;
+using PTSLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,72 @@ namespace AdminUI
 {
     public partial class assignProjectForm : Form
     {
+        private AdminFacade facade;
+        private UserModel[] teamleaders;
+        public static UserModel selectedTeamleader;
+        public static ProjectModel[] projects;
+        public static ProjectModel selectedProject;
+
+
         public assignProjectForm()
         {
             InitializeComponent();
+            facade = new AdminFacade();
+            DisplayProjects();
+            DisplayTeamleaders();
+        }
+
+        private void assignProjectForm_Load(object sender, EventArgs e)
+        {
+            cohortLabel.Text = "Assign project to " + HomeForm.selectedCohort.CohortName;
+        }
+
+        //List of projects
+        public void DisplayProjects()
+        {
+            projects = facade.GetListOfProjects();
+            projectComboBox.DataSource = projects;
+            projectComboBox.DisplayMember = "ProjectName";
+            projectComboBox.ValueMember = "ProjectID";
+        }
+
+        //List of teamleaders
+        public void DisplayTeamleaders()
+        {
+            teamleaders = facade.GetListOfTeamLeaders();
+            teamleaderComboBox.DataSource = teamleaders;
+            teamleaderComboBox.DisplayMember = "Username";
+            teamleaderComboBox.ValueMember = "ID";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cohortID;
+            cohortID = HomeForm.selectedCohort.CohortID;
+            int projectID = projects[projectComboBox.SelectedIndex].ProjectID;
+            int tlID = teamleaders[teamleaderComboBox.SelectedIndex].ID;
+
+            try
+            {
+                facade.AssignProject(
+                     startDateBox.Value,
+                     projectID,
+                     cohortID,
+                     tlID
+                  );
+                MessageBox.Show("Project added successfuly");
+                Close();  
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
     }
 }
