84384d32f595a6fb61ecfb9731cab881310d3edc
Alex-Maina <kalexmaina@gmail.com>
2022-10-09 20:53:04 +0300

    assigned project tracking

---
+++ AdminUI/viewAssignedForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace AdminUI
+{
+    partial class viewAssignedForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.projectNameLabel = new System.Windows.Forms.Label();
+            this.startDateLabel = new System.Windows.Forms.Label();
+            this.levelLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.RichTextBox();
+            this.codeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Project Code :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Level :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Description :";
+            // 
+            // projectNameLabel
+            // 
+            this.projectNameLabel.AutoSize = true;
+            this.projectNameLabel.Location = new System.Drawing.Point(476, 51);
+            this.projectNameLabel.Name = "projectNameLabel";
+            this.projectNameLabel.Size = new System.Drawing.Size(66, 25);
+            this.projectNameLabel.TabIndex = 1;
+            this.projectNameLabel.Text = "Project";
+            // 
+            // startDateLabel
+            // 
+            this.startDateLabel.AutoSize = true;
+            this.startDateLabel.Location = new System.Drawing.Point(448, 114);
+            this.startDateLabel.Name = "startDateLabel";
+            this.startDateLabel.Size = new System.Drawing.Size(49, 25);
+            this.startDateLabel.TabIndex = 2;
+            this.startDateLabel.Text = "Date";
+            // 
+            // levelLabel
+            // 
+            this.levelLabel.AutoSize = true;
+            this.levelLabel.Location = new System.Drawing.Point(136, 114);
+            this.levelLabel.Name = "levelLabel";
+            this.levelLabel.Size = new System.Drawing.Size(51, 25);
+            this.levelLabel.TabIndex = 2;
+            this.levelLabel.Text = "Level";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(343, 114);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 25);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Start Date :";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.descriptionLabel.Location = new System.Drawing.Point(187, 184);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(652, 245);
+            this.descriptionLabel.TabIndex = 3;
+            this.descriptionLabel.Text = "";
+            // 
+            // codeLabel
+            // 
+            this.codeLabel.AutoSize = true;
+            this.codeLabel.Location = new System.Drawing.Point(198, 51);
+            this.codeLabel.Name = "codeLabel";
+            this.codeLabel.Size = new System.Drawing.Size(54, 25);
+            this.codeLabel.TabIndex = 2;
+            this.codeLabel.Text = "Code";
+            // 
+            // viewAssignedForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(872, 510);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.codeLabel);
+            this.Controls.Add(this.levelLabel);
+            this.Controls.Add(this.startDateLabel);
+            this.Controls.Add(this.projectNameLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "viewAssignedForm";
+            this.Text = "viewAssignedForm";
+            this.Load += new System.EventHandler(this.viewAssignedForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label projectNameLabel;
+        private Label startDateLabel;
+        private Label levelLabel;
+        private Label label8;
+        private RichTextBox descriptionLabel;
+        private Label codeLabel;
+    }
+}
---
+++ AdminUI/viewAssignedForm.cs
@@ -0,0 +1,31 @@
+﻿using PTSLibrary.Facades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminUI
+{
+    public partial class viewAssignedForm : Form
+    {
+        private AdminFacade facade;
+        public viewAssignedForm()
+        {
+            InitializeComponent();
+            facade = new AdminFacade();
+        }
+
+        private void viewAssignedForm_Load(object sender, EventArgs e)
+        {
+            projectNameLabel.Text = HomeForm.selectedInprogress.ProjectName;
+            startDateLabel.Text = HomeForm.selectedInprogress.StartDate;
+            codeLabel.Text = HomeForm.selectedInprogress.ProjectCode;
+            levelLabel.Text = HomeForm.selectedInprogress.Level;
+        }
+    }
+}
